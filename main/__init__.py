04705f89192ddd16405af39c90d491f28c38bec8
RAM2A <160558024+RAM2A@users.noreply.github.com>
2024-04-20 13:50:52 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -14,10 +14,10 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = "24665357" #config("API_ID", default=None, cast=int)
 API_HASH = "beb7e4b83ada668fa85f9a9b56338f1d" #config("API_HASH", default=None)
-BOT_TOKEN = "6914999856:AAE7N7QocuVpQD_2FLhuomwTG5fOGAEAKHM" #config("BOT_TOKEN", default=None)
-SESSION = "BQF4XQ0Aa8ge8YCvL7ql3OI1C8FTBwkGMhZx758fqeH-9IL9V3yuAC8JsjdLuAtWjpgWRIoT4RWop40HxYr4xKcFOYPMjW7TrrJKA5k2hZp-iiPmUlrQE1cIR0mCWsno7XuunB4Eb72E34zEIEqgjTSuahEB2Nyzu5-pmlM5ZXvNB4xHUjFus5nE8yENsly2assc_LXhmnyE5CF-prkwCEixGqVAL4jV_hiuQDBBKT1HsbPOlz7RlggSeACe_U7uMt_bjvWbuvgCD6beVhi3YRXBvklsGUiCaNuuw9mz0DLDsrqmUbHbViBm-xESJFFBtFsHhSGjMBf9cWJMkYWC7hwMIXmF-wAAAABmXyPCAA" #config("SESSION", default=None)
-FORCESUB = "1002137417670" #config("FORCESUB", default=None)
-AUTH = "1717511106" #config("AUTH", default=None, cast=int)
+BOT_TOKEN = "6700540841:AAEzEG75XEQXqfTGmIvy136zVclAUBxQKOI" #config("BOT_TOKEN", default=None)
+SESSION = "BQF4XQ0AYNVos0LGcO6kXmYonSvo6KV8t1DC2HZtOhnytz_gHjkbsKguLjqlkdP9ev3PSYPf1N_H-vTM9p53tHT66bRPYveM2cYGdg_zbW8Z38NxxrNNoC_7npVbGqzUT3nuN5rtSdG5Ise_nD6UXiYdheiKEanKKY5BeX9LBzjGNzA_OjNhDumIKPAVgf96TIDBJLnSLSQsCEWWU3S7MR8PrVP1d1-Awiz-2KPb3xlwRLdHiwmhi6NpKVnoCsFsF04KwyBfCvZkRO3jv1h804FsksWPVSaiNBdHlVybaB5PjLwnqPKGUbXxI_GLB9URVIO9g7zYkVeNJPJKurpCK6npvFSOHgAAAAGPYiOpAQ" #config("SESSION", default=None)
+FORCESUB = "2137417670" #config("FORCESUB", default=None)
+AUTH = "1717511106" #config("AUTH", default=None)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
